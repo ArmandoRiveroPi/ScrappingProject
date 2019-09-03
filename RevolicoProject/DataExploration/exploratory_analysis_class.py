88fc6e08b5885af61ee06cb8ace39773256816cb
Armando Rivero <armando.rivero143@gmail.com>
2019-09-03 10:34:51 -0400

    moving to exploratory visualizations

--- RevolicoProject/DataExploration/exploratory_analysis_class.py
+++ RevolicoProject/DataExploration/exploratory_analysis_class.py
@@ -10,6 +10,7 @@ you have.
 from .exploratory_visualization_class import ExploratoryVisualization
 from .exploratory_statistics_class import ExploratoryStatistics
 import pandas as pd
+import numpy as np
 
 
 class ExploratoryAnalysis(object):
@@ -51,9 +52,21 @@ class ExploratoryAnalysis(object):
         cols = [str(col) for i, col in enumerate(self.data.columns)]
         allCorr = pd.DataFrame()
         for ind, method in enumerate(methods):
+            # Calculate the correlation
             correlation = self.data.corr(method=method)
+            # Clean the autocorrelation to not see it later
+            correlation.values[[np.arange(correlation.shape[0])]*2] = np.nan
+            # Change the names in the index to concatenate without loosing data
             subDic = {col: col + "_" + method for col in cols}
             correlation.rename(index=subDic, inplace=True)
+            # absSubDic = {col: col + "_" + 'abs' for col in cols}
+            # absCorr = correlation.rename(columns=absSubDic).copy()
+            # absCorr = absCorr.abs()
+            # # correlation = pd.concat([correlation, absCorr])
+            # correlation.reset_index().merge(absCorr, how="left",
+            #                                 left_index=True).set_index('index')
+            correlation = correlation.round(2)
             allCorr = pd.concat([allCorr, correlation])
         allCorr['group'] = group
+        allCorr['N'] = self.data.shape[0]
         return allCorr
